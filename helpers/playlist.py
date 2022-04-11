2d6d24de5a17e62fed656d6161229a684ae1d396
ToufikAmmoura <toufikammoura@gmail.com>
2022-04-11 20:21:46 +0200

    trying to solve rate limit problems

---
+++ helpers/playlist.py
@@ -0,0 +1,7 @@
+def get_playlist_tracks(sp, playlist_id):
+    results = sp.playlist_tracks(playlist_id)
+    tracks = results['items']
+    while results['next']:
+        results = sp.next(results)
+        tracks.extend(results['items'])
+    return tracks
