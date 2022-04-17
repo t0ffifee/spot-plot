2b5a6c096fcfed48ec35f6779036bba270e94e31
Toufik Ammoura <toufikammoura@gmail.com>
2022-04-18 01:20:44 +0200

    a working thing

--- helpers/spot.py
+++ helpers/spot.py
@@ -1,6 +1,6 @@
 from time import sleep
 
-def get_ids(playlist_tracks):
+def parse_ids(playlist_tracks):
     ids = []
     for track in playlist_tracks:
         try:
@@ -25,25 +25,48 @@ def get_audio_analysis(sp, tracks):
         try:
             analysis = sp.audio_analysis(tracks[i])
             track_analysis.append(analysis)
+            
+            i += 1
             if i >= len(tracks):
                 break
-            i += 1
         except:
             sleep(2)
-
     return track_analysis
 
-def get_audio_features(sp, tracks):
+def get_audio_features(sp, track_ids):
     track_features = []
     start, end = 0, 100
     while True:
         try:
-            ts = tracks[start:end]
+            ts = track_ids[start:end]
             features = sp.audio_features(ts)
             track_features.append(features)
-            if start >= len(tracks):
-                break
+
             start += 100
             end += 100
+            if start >= len(track_ids):
+                break
         except:
             sleep(2)
+    return track_features
+
+def parse_metas(playlist_tracks, METAS):
+    track_metas = []
+    for item in playlist_tracks:
+        try:
+            track = item['track']
+            m = [track[key] for key in METAS]
+            track_metas.append(m)
+        except:
+            continue
+    return track_metas
+
+def parse_features(track_features_mess, FEATURES):
+    track_features = []
+    for track in track_features_mess:
+        try:
+            f = [track[key] for key in FEATURES]
+            track_features.append(f)
+        except:
+            continue
+    return track_features
